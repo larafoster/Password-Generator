c995a45838fb16b5e1a3370f8360536b90c926cb
lara <lara@vehicle4change.org>
2021-07-08 20:42:31 -0400

    added copy clipboard and added green button

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Password-Generator
 03 JavaScript: Password Generator
+
+
+####Copy to Clipboard bonus
+I found this code here
+https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
+Using execCommand()
+The document.execCommand() method's "cut" and "copy" commands can be used to replace the clipboard's current contents with the selected material. These commands can be used without any special permission if you are using them in a short-lived event handler for a user action (for example, a click handler).
+
+For example, suppose you've got a popup that includes the following HTML:
+
+<input id="input" type="text"/>
+<button id="copy">Copy</button>
+Copy to Clipboard
+To make the "copy" button copy the contents of the <input> element, you can use code like this:
+
+function copy() {
+  var copyText = document.querySelector("#input");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+document.querySelector("#copy").addEventListener("click", copy);
+Copy to Clipboard
+Because the execCommand() call is inside a click event handler, you don't need any special permissions here.
--- script.js
+++ script.js
@@ -1,5 +1,94 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector ('#generate');
+
+function generatePassword () {
+  // user selections
+
+  var uppercaseOption = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  var lowercaseOption = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  var numberOption = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialCharOption = [
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*',
+    '(',
+    ')',
+    '-',
+    '_',
+    '=',
+    '+',
+  ];
+
+  // empty array for input and output
+  var userOptions = [];
+  var results = [];
+
+  //-----------------------------------------------------------------
+  var howMany = prompt (
+    'How many characters do you want your password to be? Choose between 8 and 128?'
+  );
+  var numbers = confirm ('Do you want to include numbers??');
+  var uppercase = confirm ('Do you want to include UPPERCASE letters?');
+  var lowercase = confirm ('Do you want to include lowercase letters?');
+  var special = confirm ('Would you like to include special?');
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +101,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//added copy to clipboard feature
+function copy () {
+  var copyText = document.querySelector ('#password');
+  copyText.select ();
+  document.execCommand ('copy');
+}
+
+document.querySelector ('#copy').addEventListener ('click', copy);
